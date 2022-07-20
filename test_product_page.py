a53176c5d5de8df456bda02951d78f73c8559c6d
klymentyev <klymentyevng@gmail.com>
2022-07-20 21:53:28 +0300

    review marks added

--- test_product_page.py
+++ test_product_page.py
@@ -46,6 +46,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -53,6 +54,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.should_be_login_link()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def  test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = BasketPage(browser, link)
@@ -78,6 +80,7 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(str(time.time()) + "@gmail.com", "superpassword")
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
