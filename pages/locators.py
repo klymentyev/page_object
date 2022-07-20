ce3ca8066965f6143dd3bb7b91463f4f65134288
klymentyev <klymentyevng@gmail.com>
2022-07-20 18:09:11 +0300

    add product to basket test added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, "")
+    BOOK_NAME_MESSAGE = (By.CSS_SELECTOR, "")
+    PRICE = (By.CSS_SELECTOR, "")
+    PRICE_MESSAGE = (By.CSS_SELECTOR, "")
